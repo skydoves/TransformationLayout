6c8be028daa715035f2f41862d4d4a5e67564dce
skydoves <skydoves@naver.com>
2023-03-05 17:12:26 +0900

    Migrate to maven publish

---
+++ transformationlayout/build.gradle.kts
@@ -0,0 +1,69 @@
+// Designed and developed by 2020 skydoves (Jaewoong Eum)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+@file:Suppress("UnstableApiUsage")
+
+import com.skydoves.tranformationlayout.Configuration
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+  id(libs.plugins.android.library.get().pluginId)
+  id(libs.plugins.kotlin.android.get().pluginId)
+  id(libs.plugins.kotlin.parcelize.get().pluginId)
+}
+
+rootProject.extra.apply {
+  set("PUBLISH_GROUP_ID", Configuration.artifactGroup)
+  set("PUBLISH_ARTIFACT_ID", "transformationlayout")
+  set("PUBLISH_VERSION", rootProject.extra.get("rootVersionName"))
+}
+
+apply(from ="${rootDir}/scripts/publish-module.gradle")
+
+android {
+  compileSdk = Configuration.compileSdk
+  defaultConfig {
+    minSdk = Configuration.minSdk
+    targetSdk = Configuration.targetSdk
+  }
+
+  resourcePrefix = "transformation"
+
+  buildFeatures {
+    viewBinding = true
+  }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+
+  kotlinOptions {
+    jvmTarget = libs.versions.jvmTarget.get()
+  }
+
+  lint {
+    abortOnError = false
+  }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+  kotlinOptions.freeCompilerArgs += listOf(
+    "-Xexplicit-api=strict"
+  )
+}
+
+dependencies {
+  implementation(libs.androidx.material)
+}
