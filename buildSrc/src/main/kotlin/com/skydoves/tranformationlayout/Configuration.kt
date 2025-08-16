6d8a67c1f3e702de2a23f232cf949cd0f98efe94
skydoves <skydoves@naver.com>
2025-08-16 15:17:28 +0900

    Prepare to release 1.1.6

--- buildSrc/src/main/kotlin/com/skydoves/tranformationlayout/Configuration.kt
+++ buildSrc/src/main/kotlin/com/skydoves/tranformationlayout/Configuration.kt
@@ -17,14 +17,14 @@
 package com.skydoves.tranformationlayout
 
 object Configuration {
-  const val compileSdk = 34
-  const val targetSdk = 34
+  const val compileSdk = 36
+  const val targetSdk = 36
   const val minSdk = 21
   const val majorVersion = 1
   const val minorVersion = 1
-  const val patchVersion = 5
+  const val patchVersion = 6
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 15
+  const val versionCode = 16
   const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
   const val artifactGroup = "com.github.skydoves"
 }
